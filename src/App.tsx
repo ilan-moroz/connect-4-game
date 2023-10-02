dfff0c1f813bb50b04563b493d8f196b83881edf
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-10-02 13:50:32 +0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import GameBoard from './components/GameBoard/GameBoard';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <GameBoard />
     </div>
   );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import classes from './Header.module.css';
+
+const Header = () => {
+  return <h1 className={classes.header}>Connect 4</h1>;
+};
+
+export default Header;
