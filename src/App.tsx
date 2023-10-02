07ba8390b1225a8ee87496f244b81975473cfab7
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-10-02 15:19:48 +0300

    handleClick

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import GameBoard from './components/GameBoard/GameBoard';
 import Header from './components/Header/Header';
 
 function App() {
+  const [player, setPlayer] = useState<string>('player1');
+
   return (
     <div className="App">
       <Header />
--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -3,17 +3,43 @@ import classes from './GameBoard.module.css';
 
 const GameBoard = () => {
   const [board, setBoard] = useState(createEmptyBoard());
+  const [currentPlayer, setCurrentPlayer] = useState('red');
 
   function createEmptyBoard() {
     return Array.from({ length: 6 }, () => Array(7).fill(null));
   }
+
+  const handleClick = (colIndex: number) => {
+    // Copy the current board state
+    const newBoard = [...board];
+
+    // Find the lowest empty cell in the clicked column
+    for (let i = newBoard.length - 1; i >= 0; i--) {
+      if (!newBoard[i][colIndex]) {
+        newBoard[i][colIndex] = currentPlayer;
+        break;
+      }
+    }
+    // Update the board state
+    setBoard(newBoard);
+    // Switch to the other player
+    setCurrentPlayer(currentPlayer === 'red' ? 'yellow' : 'red');
+  };
 
   return (
     <div className={classes.board}>
       {board.map((row, rowIndex) => (
         <div key={rowIndex} className={classes.row}>
           {row.map((cell, colIndex) => (
-            <div key={colIndex} className={classes.cell}></div>
+            <div
+              key={colIndex}
+              className={classes.cell}
+              onClick={() => handleClick(colIndex)}
+            >
+              {cell && (
+                <div className={`${classes.disc} ${classes[cell]}`}></div>
+              )}
+            </div>
           ))}
         </div>
       ))}
